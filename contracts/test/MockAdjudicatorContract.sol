01731d63252cd72163ada00d78dd6611d28660e0
Shuhei Hiya <syuhei176@gmail.com>
2020-05-13 16:52:28 +0900

    property should be stored as hash in Adjudicator Contract

--- contracts/test/MockAdjudicatorContract.sol
+++ contracts/test/MockAdjudicatorContract.sol
@@ -15,5 +15,8 @@ contract MockAdjudicationContract {
     function isDecided(types.Property memory _property) public returns (bool) {
         return !fail;
     }
-    function setPredicateDecision(bytes32 _gameId, bool _decision) public {}
+    function setPredicateDecision(
+        types.Property memory _property,
+        bool _decision
+    ) public {}
 }
